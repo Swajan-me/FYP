35ed2b5ba32dac1c355d5c5bccead536a3874f8f
Swajan-me <swajanshrestha3180@gmail.com>
2025-05-03 12:37:22 +0545

    Add files via upload

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -2,23 +2,20 @@ extends Control
 
 const CHAR_SELECTOR_SCENE := preload("res://scenes/ui/character_selector.tscn")
 
+@export var music: AudioStream
 
 # 
 func _ready() -> void:
+	MusicPlayer.play(music, true)
 	get_tree().paused = false
 
 func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_packed(CHAR_SELECTOR_SCENE)
 	#print("New game")
 
-func _on_settings_pressed() -> void:
-	var current_db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
-	if current_db <= -30:  # muted
-		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), 0)  # unmute
-	else:
-		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -30)  # mute
-		
-	print("Music volume toggled. Current dB:", AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+func _on_tutorial_pressed() -> void:
+	var file_path = "C:/Users/swaja/Desktop/FYP/the-game/Tutorial.pptx"
+	OS.shell_open(file_path)
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
